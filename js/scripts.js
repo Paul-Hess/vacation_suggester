32a6a1fea092a5192cc8c4016ed633f076ab4166
Paul-Hess <pauljacksonhess@gmail.com>
2016-01-15 14:48:56 -0800

    flesh out satellite pages with content/styling

--- js/scripts.js
+++ js/scripts.js
@@ -24,11 +24,11 @@ $(document).ready(function() {
 		var forest = $('.radio')[2].checked || $('.radio')[5].checked || $('.radio')[8].checked;
 
 		if (montane) {
-			location.href = "pages/montane.html"
+			location.href = "montane.html"
 		} else if (desert) {
-			location.href = "pages/desert.html"
+			location.href = "desert.html"
 		} else if (forest) {
-			location.href = "pages/forest.html"
+			location.href = "forest.html"
 		} else {
 			alert("Please make a valid selection")
 			location.href = "pages/wild.html"
@@ -37,4 +37,45 @@ $(document).ready(function() {
 		return false;
 		event.preventDefault()	;
 	});
+
+	$('form#cityPlaces').submit(function(event){
+		var europe = $('.radio')[0].checked || $('.radio')[5].checked || $('.radio')[6].checked;
+		var asia = $('.radio')[1].checked || $('.radio')[4].checked || $('.radio')[8].checked;
+		var brazil = $('.radio')[2].checked || $('.radio')[3].checked || $('.radio')[7].checked;
+
+		if (europe) {
+			location.href = "europe.html"
+		} else if (asia) {
+			location.href = "asia.html"
+		} else if (brazil) {
+			location.href = "brazil.html"
+		} else {
+			alert("Please make a valid selection")
+			location.href = "city.html"
+			
+		}
+		return false;
+		event.preventDefault()	;
+	});
+
+	$('form#beachPlaces').submit(function(event){
+		var oregon = $('.radio')[0].checked || $('.radio')[5].checked || $('.radio')[6].checked;
+		var tropical = $('.radio')[1].checked || $('.radio')[4].checked || $('.radio')[8].checked;
+		var cali = $('.radio')[2].checked || $('.radio')[3].checked || $('.radio')[7].checked;
+
+		if (oregon) {
+			location.href = "oregon.html"
+		} else if (tropical) {
+			location.href = "tropical.html"
+		} else if (cali) {
+			location.href = "california.html"
+		} else {
+			alert("Please make a valid selection")
+			location.href = "beach.html"
+			
+		}
+		return false;
+		event.preventDefault()	;
+	});
+
 });
